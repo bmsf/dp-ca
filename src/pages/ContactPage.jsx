d1c6f716b30638b1378d2eb4f36f62c4c1f218f8
bmsf <fromreide94@gmail.com>
2023-06-09 22:36:32 +0200

    first finished draft

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,72 +1,105 @@
 import Layout from '../components/Layout';
 
 const ContactPage = () => {
+	const handleFormSubmit = (event) => {
+		const [formData, setFormData] = useState({
+			fullName: '',
+			subject: '',
+			email: '',
+			body: '',
+		});
+
+		event.preventDefault();
+		console.log(formData);
+	};
 	return (
-		<div>
-			<Layout>
-				<section className='bg-gray-200 p-8'>
-					<div className='container mx-auto'>
-						<h2 className='text-2xl font-bold mb-4'>Contact Us</h2>
-						<form>
-							<div className='mb-4'>
-								<label
-									className='block text-gray-700 font-semibold mb-2'
-									htmlFor='fullName'
-								>
-									Full Name
-								</label>
-								<input
-									type='text'
-									id='fullName'
-									className='w-full border border-gray-300 rounded py-2 px-3 text-gray-700'
-								/>
-							</div>
-							<div className='mb-4'>
-								<label
-									className='block text-gray-700 font-semibold mb-2'
-									htmlFor='subject'
-								>
-									Subject
-								</label>
-								<input
-									type='text'
-									id='subject'
-									className='w-full border border-gray-300 rounded py-2 px-3 text-gray-700'
-								/>
-							</div>
-							<div className='mb-4'>
-								<label
-									className='block text-gray-700 font-semibold mb-2'
-									htmlFor='email'
-								>
-									Email
-								</label>
-								<input
-									type='email'
-									id='email'
-									className='w-full border border-gray-300 rounded py-2 px-3 text-gray-700'
-								/>
-							</div>
-							<div className='mb-4'>
-								<label
-									className='block text-gray-700 font-semibold mb-2'
-									htmlFor='body'
-								>
-									Body
-								</label>
-								<textarea
-									id='body'
-									className='w-full border border-gray-300 rounded py-2 px-3 text-gray-700 h-32 resize-none'
-								></textarea>
-							</div>
-							<button className='bg-blue-500 text-white px-4 py-2 rounded'>
-								Send Message
-							</button>
-						</form>
+		<Layout>
+			<div className='container mx-auto py-8'>
+				<h2 className='text-2xl font-bold mb-4'>Contact Us</h2>
+				<form onSubmit={handleFormSubmit}>
+					<div className='mb-4'>
+						<label
+							className='block text-gray-700 font-semibold mb-2'
+							htmlFor='fullName'
+						>
+							Full Name
+						</label>
+						<input
+							type='text'
+							id='fullName'
+							name='fullName'
+							className='w-full border border-gray-300 rounded py-2 px-3 text-gray-700'
+							minLength='3'
+							required
+						/>
+						<div className='text-red-500'>
+							<span>*</span> Required field
+						</div>
 					</div>
-				</section>
-			</Layout>
-		</div>
+					<div className='mb-4'>
+						<label
+							className='block text-gray-700 font-semibold mb-2'
+							htmlFor='subject'
+						>
+							Subject
+						</label>
+						<input
+							type='text'
+							id='subject'
+							name='subject'
+							className='w-full border border-gray-300 rounded py-2 px-3 text-gray-700'
+							minLength='3'
+							required
+						/>
+						<div className='text-red-500'>
+							<span>*</span> Required field
+						</div>
+					</div>
+					<div className='mb-4'>
+						<label
+							className='block text-gray-700 font-semibold mb-2'
+							htmlFor='email'
+						>
+							Email
+						</label>
+						<input
+							type='email'
+							id='email'
+							name='email'
+							className='w-full border border-gray-300 rounded py-2 px-3 text-gray-700'
+							required
+						/>
+						<div className='text-red-500'>
+							<span>*</span> Required field
+						</div>
+					</div>
+					<div className='mb-4'>
+						<label
+							className='block text-gray-700 font-semibold mb-2'
+							htmlFor='body'
+						>
+							Body
+						</label>
+						<textarea
+							id='body'
+							name='body'
+							className='w-full border border-gray-300 rounded py-2 px-3 text-gray-700 h-32 resize-none'
+							minLength='3'
+							required
+						></textarea>
+						<div className='text-red-500'>
+							<span>*</span> Required field
+						</div>
+					</div>
+					<button
+						className='bg-blue-500 text-white px-4 py-2 rounded'
+						type='submit'
+					>
+						Submit
+					</button>
+				</form>
+			</div>
+		</Layout>
 	);
 };
 
