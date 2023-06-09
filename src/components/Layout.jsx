29c58b2e64478ac0084bce23076ae2362fa65532
bmsf <fromreide94@gmail.com>
2023-06-09 22:45:09 +0200

    fixed height on layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
 	return (
-		<div className='flex flex-col justify-between h-screen'>
+		<div className='flex flex-col justify-between min-h-screen'>
 			<Navbar />
 			{children}
 			<Footer />
